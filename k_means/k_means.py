f746a084813e483ee5be689c2cbdab79b48806ae
menbu <menbuas69@gmail.com>
2023-09-16 16:59:45 +0200

    started kmeans fit implementation

--- k_means/k_means.py
+++ k_means/k_means.py
@@ -1,16 +1,19 @@
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
+
+
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
 
 
 class KMeans:
-    
-    def __init__():
+
+    def __init__(self, k=2):
         # NOTE: Feel free add any hyperparameters 
         # (with defaults) as you see fit
-        pass
-        
+        self.k = k
+        self.centroids_coordinates = np.empty((self.k,))
+
     def fit(self, X):
         """
         Estimates parameters for the classifier
@@ -19,9 +22,32 @@ class KMeans:
             X (array<m,n>): a matrix of floats with
                 m rows (#samples) and n columns (#features)
         """
-        # TODO: Implement
-        raise NotImplemented()
-    
+
+        # if there are more centroids than samples simply set the centroids list as the samples list
+        if X.shape[0] <= self.k:
+            self.centroids_coordinates = X.to_numpy()
+            return
+
+        # define arbitrarily k centroids
+        self.centroids_coordinates = X.iloc[:self.k].to_numpy()
+
+        # array which for each sample contains the index of the closest centroid, in the centroid list of the class
+        centroids_assignations = np.zeros(X.shape[0], dtype=int)
+
+        for sample_index, row in X.iterrows():
+            min_dist = euclidean_distance(row, self.centroids_coordinates[0])
+            closest_centroid = 0
+            for id_centroid, centroid in enumerate(self.centroids_coordinates[1:], start=1):
+                dist = euclidean_distance(row, centroid)
+                if dist < min_dist:
+                    min_dist = dist
+                    closest_centroid = id_centroid
+            centroids_assignations[sample_index] = closest_centroid
+
+
+        # for each cluster compute the centroid
+        # do it again until the centroids distance from himself doesnt change
+
     def predict(self, X):
         """
         Generates predictions
@@ -38,9 +64,7 @@ class KMeans:
             there are 3 clusters, then a possible assignment
             could be: array([2, 0, 0, 1, 2, 1, 1, 0, 2, 2])
         """
-        # TODO: Implement 
-        raise NotImplemented()
-    
+
     def get_centroids(self):
         """
         Returns the centroids found by the K-mean algorithm
@@ -57,10 +81,8 @@ class KMeans:
         ])
         """
         pass
-    
-    
-    
-    
+
+
 # --- Some utility functions 
 
 def euclidean_distance(x, y):
@@ -79,12 +101,13 @@ def euclidean_distance(x, y):
     """
     return np.linalg.norm(x - y, ord=2, axis=-1)
 
+
 def cross_euclidean_distance(x, y=None):
     """
     
     
     """
-    y = x if y is None else y 
+    y = x if y is None else y
     assert len(x.shape) >= 2
     assert len(y.shape) >= 2
     return euclidean_distance(x[..., :, None, :], y[..., None, :, :])
@@ -105,14 +128,14 @@ def euclidean_distortion(X, z):
     assert len(X.shape) == 2
     assert len(z.shape) == 1
     assert X.shape[0] == z.shape[0]
-    
+
     distortion = 0.0
     clusters = np.unique(z)
     for i, c in enumerate(clusters):
         Xc = X[z == c]
         mu = Xc.mean(axis=0)
         distortion += ((Xc - mu) ** 2).sum(axis=1)
-        
+
     return distortion
 
 
@@ -135,7 +158,7 @@ def euclidean_silhouette(X, z):
     assert len(X.shape) == 2
     assert len(z.shape) == 1
     assert X.shape[0] == z.shape[0]
-    
+
     # Compute average distances from each x to all other clusters
     clusters = np.unique(z)
     D = np.zeros((len(X), len(clusters)))
@@ -146,12 +169,11 @@ def euclidean_silhouette(X, z):
             d = cross_euclidean_distance(X[in_cluster_a], X[in_cluster_b])
             div = d.shape[1] - int(i == j)
             D[in_cluster_a, j] = d.sum(axis=1) / np.clip(div, 1, None)
-    
+
     # Intra distance 
     a = D[np.arange(len(X)), z]
     # Smallest inter distance 
     inf_mask = np.where(z[:, None] == clusters[None], np.inf, 0)
     b = (D + inf_mask).min(axis=1)
-    
+
     return np.mean((b - a) / np.maximum(a, b))
-  
